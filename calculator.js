5ea5ce2c99a8b3258a0117a85ee244e820b8002c
Jomagene <138574423+Jomagene@users.noreply.github.com>
2023-09-27 13:00:40 +0300

    function calculator
    * Definition of functions
    
    * Correction1
    
    * Update function2
    
    * version2

--- calculator.js
+++ calculator.js
@@ -1,3 +1,28 @@
 let number1     = parseInt(Readline());
 let operator    = Readline();
 let number2     = parseInt(Readline());
+
+function add(number1, number2){
+  return (number1+number2);
+}
+function diff(number1, number2){
+  return (number1-number2);
+}
+function mult(number1, number2){
+  return (number1*number2);
+}
+function devide(number1, number2){
+  return (number1/number2);
+}
+
+
+function calculator(number1, operator, number2){
+  let result=0;
+  switch (operator){
+    case '+': result=add(number1, number2);
+    case '-': result=diff(number1, number2);
+    case '/': result=devide(number1, number2);
+    case '*': result=mult(number1, number2);
+  }
+  return result;
+}
